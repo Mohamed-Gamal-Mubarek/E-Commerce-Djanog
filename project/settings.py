e0e73b4eae09af1c9cb35dc4050b4428f84b0748
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-15 17:20:20 +0200

    adding medai folder and set configuer to it to upload any media or image in application

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # IT TAKES BOOLEAN (' I WIL COMEBACK AGAIN TO SEE IT ')
 CORS_ALLOW_ALL_ORIGINS = True
 
