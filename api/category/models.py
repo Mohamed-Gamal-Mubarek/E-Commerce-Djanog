eb6694f0dafad8e3d9b8a6a774908dbd8b135d85
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-15 20:40:18 +0200

    adding method like constructor in modle to seee the name when model is created

--- api/category/models.py
+++ api/category/models.py
@@ -7,3 +7,8 @@ class Category(models.Model):
     description = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # RETURN THE NAME OF OBJECT => CONSTRUCTOR
+    def __str__(self):
+        return self.name
+    # RETURN THE NAME OF OBJECT => CONSTRUCTOR
